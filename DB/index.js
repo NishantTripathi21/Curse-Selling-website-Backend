290ace215f51e9ac40c46568a51e5ab9b487043f
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-08-02 17:17:52 +0530

    fixed

--- DB/index.js
+++ DB/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/CourseSellingJWT');
+mongoose.connect('');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
